11e0a45e81d3c508ae30ccd942b33562b64d51bb
allankimanzi2 <allankim457@gmail.com>
2023-08-22 06:14:47 +0000

    NodeJSBasics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Allan Kimanzi <https://github.com/allankimanzi2>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
